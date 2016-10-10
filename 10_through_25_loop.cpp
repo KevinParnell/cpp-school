3f2d2f0912ccf78513e3812d4612a80a8defd3d4
Kevin Parnell <kpbballer@gmail.com>
2016-10-10 18:00:33 -0400

    added another unfinished program

---
+++ 10_through_25_loop.cpp
@@ -0,0 +1,9 @@
+// This program is an input validation loop that asks the user to enter a number in the range of 10 through 25
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const int MIN_NUMBER = 10
+  const int MAX_NUMBER = 25
+}
